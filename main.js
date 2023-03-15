86c9ab1c3440247e14fa564f8f3bd6951ed8fa53
Hollas <nicholasolajide@gmail.com>
2023-03-15 21:51:07 +0100

    media query

---
+++ main.js
@@ -0,0 +1,46 @@
+const hamburger = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".sidebar");
+
+hamburger.addEventListener('click', () => {
+  // Toggle the class that shows the sidebar
+  sidebar.classList.toggle('show-sidebar');
+
+  // Add a close icon to the sidebar when it is visible
+  if (sidebar.classList.contains('show-sidebar')) {
+    const closeIcon = document.createElement('span');
+    closeIcon.classList.add('close-sidebar');
+    closeIcon.innerHTML = '&times;';
+    sidebar.appendChild(closeIcon);
+  } else {
+    // Remove the close icon when the sidebar is hidden
+    sidebar.removeChild(sidebar.lastChild);
+  }
+});
+// const logo = document.querySelector(".logo");
+
+// hamburger.addEventListener("click", () => {
+//   menu.classList.toggle("show");
+// });
+
+// window.addEventListener("resize", () => {
+//   if (window.innerWidth >= 768) {
+//     menu.classList.remove("show");
+//   }
+// });
+
+// window.addEventListener("load", () => {
+//   if (window.innerWidth < 768) {
+//     hamburger.style.display = "block";
+//     menu.style.display = "none";
+//   }
+// });
+
+// const clickMeButton = document.getElementById("click-me");
+
+// clickMeButton.addEventListener("click", function() {
+//   const innerElement1 = menu;
+//   const innerElement2 = hamburger;
+  
+//   innerElement1.appendChild(innerElement2);
+//   innerElement2.style.display = "block";
+// });
